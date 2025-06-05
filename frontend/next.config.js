8dd809996b3448dbb3bd9f6518ebe2d3894ce63e
ebaenamar <e.baena@northeastern.edu>
2025-06-05 08:53:55 -0400

    chore: deploy memories

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,10 +6,17 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*` : 'http://localhost:5000/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*` : 'http://localhost:5001/api/:path*',
       },
     ];
   },
+  // Configuración para Vercel
+  distDir: '.next',
+  // Asegurarse de que las imágenes funcionen correctamente
+  images: {
+    domains: ['localhost'],
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
